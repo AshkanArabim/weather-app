b8e4015c42daf937360a2de4f1675550d7ca581f
AshkanArabim <ashkan.arabim@gmail.com>
2022-12-24 17:01:06 -0700

    add sample hourly temperature graph

--- src/index.js
+++ src/index.js
@@ -1,2 +1,13 @@
 import dom from "./dom.js";
+import data from "./data.js";
 import css from "./style.css";
+
+console.log("hellow");
+
+const graphs = document.querySelectorAll(".hourly-chart > *");
+
+let h = 0;
+for (let graph of graphs) {
+  h += 10
+  graph.style.height = `${h}px`;
+}
