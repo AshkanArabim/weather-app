edb4ac6795419f3ea0b68eabd472d406f390ed3b
AshkanArabim <ashkan.arabim@gmail.com>
2023-01-04 13:18:33 -0700

    add polution forecast function

--- src/dom.js
+++ src/dom.js
@@ -11,12 +11,39 @@ function render() {
 
 async function data() {
   const key = "d4c3301e7c7b03c479ca4c063371796e";
-  return await getCurrentWeather("el paso", key);
+  return await getPollution("el paso", key);
 }
 
-async function getCurrentWeather(city, key) {
+async function getCurrent(city, key) {
   // TODO: (later) add support for state and country names
   return await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`)
+    .then((response) => response.json())
+}
+
+// convert city name to latitude and longitude --> sometimes useful
+async function getCoords(cityName, key) {
+  return await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${key}`)
+    .then((response) => response.json())
+    .then(response => {
+      return [response[0].lat, response[0].lon];
+    })
+}
+
+async function getPollution(city, key) {
+  // TODO: (later) add support for state and country names
+  return await getCoords(city, key)
+  .then(coords => fetch(`http://api.openweathermap.org/data/2.5/air_pollution/forecast?lat=${coords[0]}&lon=${coords[1]}&appid=${key}`))
+}
+
+async function getHourly(city, key) {
+  // TODO: (later) add support for state and country names
+  return await fetch(`https://pro.openweathermap.org/data/2.5/forecast/hourly?q=${city}&appid=${key}`)
+    .then((response) => response.json())
+}
+
+async function getWeekly(city, key) {
+  // TODO: (later) add support for state and country names
+  return await fetch(`api.openweathermap.org/data/2.5/forecast/daily?q=${city}&cnt=7&appid=${key}`)
     .then((response) => response.json())
 }
 
